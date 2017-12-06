7ebdcb111f7a1e27efe5589be9b14e7a810c8217
Jake Acalin <codewithjake@gmail.com>
2017-12-06 10:09:09 -0500

    added getindex

---
+++ JavaScript/getindex.js
@@ -0,0 +1,38 @@
+/*
+
+Return the lowest index at which a value (second argument)
+should be inserted into an array (first argument) once it has been sorted.
+The returned value should be a number.
+
+
+*/
+
+
+function getIndexToIns(arr, num) {
+
+  // sorted arr, small to large.
+  var sorted = arr.sort(function(a,b){
+    return a-b;
+
+  });
+
+  // init list which will contain ith position of sorted list.
+  var index = [];
+
+  // loop through sorted list, and push any i vals >= num.
+  for(var i=0, len=sorted.length; i<len; i++){
+    if(num > sorted[i]){
+      index.push(i+1);
+    }else if(num === sorted[i] )
+      index.push(i);
+  }
+
+  // slice all vals in list except for the last. 
+  var result = index.slice(-1);
+
+  // return index
+  return result[0];
+
+}
+
+getIndexToIns([3, 10, 5], 3);
--- JavaScript/seekdestroy.js
+++ JavaScript/seekdestroy.js
@@ -1,23 +1,32 @@
 function destroyer(arr) {
 
+
+  // convert arguments into an array
   var args = Array.from(arguments);
 
+  // first list contains array, second contains all other passed args.
   var list1 = args[0], list2 = args.slice(1);
 
+  // create callback func for .filter
   function filter(value){
 
+    // loop through l2. If l1 value = l2[i] r false, otherwise r value.
     for(var i=0, len=list2.length; i<len; i++){
 
       if(value === list2[i]){
-        return;
+        return false;
       }
     }
       return value;
 
     }
 
+  // stores results from callback into var results.
   var result = list1.filter(filter);
 
+  // return array without given values.
   return result;
 
 }
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
